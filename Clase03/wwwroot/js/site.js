765b2a68db99c85c2a1e0b25fd15a1d2b8aee11c
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-04-02 07:46:22 -0500

    Agregar archivos de proyecto.

---
+++ Clase03/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function Saluda() {
+    alert("hola");
+    document.getElementById("input1").value = "Hola Santiago";
+
+
+}
+
+
+function Despidete() {
+    alert("adios");
+    document.getElementById("input1").value = "Chao Santiago";
+}
+
+

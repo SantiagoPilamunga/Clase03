b31827852b70c4ef379845cc0829333d2755d036
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-04-02 08:04:05 -0500

    js-buton index bases

--- Clase03/wwwroot/js/site.js
+++ Clase03/wwwroot/js/site.js
@@ -2,17 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    alert("Estoy listo");
+
+    $("#Boton_saluda").click(function () {
+        Saluda();
+    });
+
+    $("#Boton_despidete").click(function () {
+        Despidete();
+    });
+});
 function Saluda() {
     alert("hola");
     document.getElementById("input1").value = "Hola Santiago";
-
-
+    $("#input2").val("Hola SJPC Jquery");
 }
 
 
 function Despidete() {
     alert("adios");
     document.getElementById("input1").value = "Chao Santiago";
+    $("#input2").val("Chao SJPC Jquery");
 }
 
 
